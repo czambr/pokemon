2268dd39d57f17b13bd9a24f17f49169c842edd2
Carlos Zambrano <>
2023-12-01 08:25:01 -0500

    Favorites Pages

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,9 +1,49 @@
+import { useEffect, useState } from 'react';
+import { Card, Grid } from '@nextui-org/react';
+
 import { Layout } from '../../components/layouts';
+import { NoFavorites } from '../../components/ui';
+import { localFavorites } from '../../utils';
 
 const FavoritePages = () => {
+    const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+
+    useEffect(() => {
+        setFavoritePokemons(localFavorites.pokemons());
+    }, []);
+
     return (
         <Layout title='Pokemones - Favorites'>
-            <h1>Favorites</h1>
+            {favoritePokemons.length === 0 ? (
+                <NoFavorites />
+            ) : (
+                <Grid.Container
+                    gap={2}
+                    direction='row'
+                    justify='flex-start'
+                >
+                    {favoritePokemons.map(id => (
+                        <Grid
+                            xs={6}
+                            sm={2}
+                            xl={1}
+                            key={id}
+                        >
+                            <Card
+                                hoverable
+                                clickable
+                                css={{ padding: '10px' }}
+                            >
+                                <Card.Image
+                                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                                    width={'100px'}
+                                    height={140}
+                                />
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid.Container>
+            )}
         </Layout>
     );
 };
